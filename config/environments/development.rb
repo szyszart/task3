c4d0ca3b5d8390dc84b638e20502c43c210defe0
Krzysztof Kusiak <szyszart@gmail.com>
2010-11-02 23:25:35 +0100

    minor changes

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -1,5 +1,5 @@
 # Settings specified here will take precedence over those in config/environment.rb
-
+#
 # In the development environment your application's code is reloaded on
 # every request.  This slows down response time but is perfect for development
 # since you don't have to restart the webserver when you make code changes.
@@ -21,11 +21,13 @@ config.action_mailer.smtp_settings = {
  :address        => 'smtp.gmail.com',
  :port           => 587,
  :authentication => :plain,
+ :user_name      => '',
+ :password       => ''
 }
 
 config.action_mailer.default_url_options = { :host => 'localhost:3000' }
 config.action_mailer.delivery_method = :smtp
-config.action_mailer.perform_deliveries = true
+config.action_mailer.perform_deliveries = false
 config.action_mailer.raise_delivery_errors = true
 config.action_mailer.default_charset = "utf-8"
 
--- config/environments/production.rb
+++ config/environments/production.rb
@@ -1,5 +1,5 @@
 # Settings specified here will take precedence over those in config/environment.rb
-
+#
 # The production environment is meant for finished, "live" apps.
 # Code is not reloaded between requests
 config.cache_classes = true
@@ -9,20 +9,21 @@ config.action_controller.consider_all_requests_local = false
 config.action_controller.perform_caching             = true
 config.action_view.cache_template_loading            = true
 
-config.action_mailer.default_url_options = { :host => 'heroku.com' }
-### ActionMailer Config
-# Setup for production - deliveries, no errors raised
-config.action_mailer.delivery_method = :smtp
-config.action_mailer.perform_deliveries = true
-config.action_mailer.raise_delivery_errors = true
-config.action_mailer.default_charset = "utf-8"
+config.action_mailer.default_url_options = { :host => 'warm-samurai-61.heroku.com' }
 
 config.action_mailer.smtp_settings = {
  :enable_starttls_auto => true,
  :address        => 'smtp.gmail.com',
  :port           => 587,
  :authentication => :plain,
+ :user_name      => '',
+ :password       => ''
 }
+
+config.action_mailer.delivery_method = :smtp
+config.action_mailer.perform_deliveries = true
+config.action_mailer.raise_delivery_errors = true
+config.action_mailer.default_charset = "utf-8"
 
 # See everything in the log (default is :info)
 # config.log_level = :debug
